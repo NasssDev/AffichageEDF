6e5d7b158251b85987e35c43f6f452180756c8de
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-05-13 19:11:32 +0200

    fix script

--- install_pdftk.sh
+++ install_pdftk.sh
@@ -4,15 +4,12 @@
 wget https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk-2.02-1.debian-9.amd64.deb
 
 # Extrait les fichiers du paquet .deb
-tar -x pdftk-2.02-1.debian-9.amd64.deb
-
-# Extrai le fichier système
-tar -xvf data.tar.xz
+dpkg-deb -x pdftk-2.02-1.debian-9.amd64.deb pdftk-package
 
 # Copie le binaire pdftk vers un emplacement accessible
-cp usr/bin/pdftk ./bin/
+cp pdftk-package/usr/bin/pdftk ./bin/
 
 # Nettoyage des fichiers temporaires
-rm -rf usr data.tar.xz debian-binary pdftk-2.02-1.debian-9.amd64.deb
+rm -rf pdftk-package pdftk-2.02-1.debian-9.amd64.deb
 
 echo "pdftk a été installé avec succès."
