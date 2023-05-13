d29b8a8ae717e716177277a25034dd79e21f1ed9
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-05-13 19:17:10 +0200

    fix script

--- install_pdftk.sh
+++ install_pdftk.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Télécharge le binaire pdftk précompilé
-wget https://github.com/sumatrapdfreader/sumatrapdf/releases/download/3.3.3/PDFTK.zip
+wget https://github.com/sidorares/pdftk/releases/download/v2.2.1/pdftk-2.2.1-linux-x86_64.tar.gz
 
 # Extrait le contenu de l'archive
-unzip PDFTK.zip -d pdftk-package
+tar -xzf pdftk-2.2.1-linux-x86_64.tar.gz
 
 # Copie le binaire pdftk vers un emplacement accessible
-cp pdftk-package/pdftk/bin/pdftk ./bin/
+cp pdftk-2.2.1-linux-x86_64/bin/pdftk ./bin/
 
 # Nettoyage des fichiers temporaires
-rm -rf pdftk-package PDFTK.zip
+rm -rf pdftk-2.2.1-linux-x86_64 pdftk-2.2.1-linux-x86_64.tar.gz
 
 echo "pdftk a été installé avec succès."
