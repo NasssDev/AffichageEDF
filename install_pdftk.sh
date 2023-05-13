bc130ae8e7649e294a42e161e1a5ba2e79e13a49
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-05-13 19:18:57 +0200

    fix script

--- install_pdftk.sh
+++ install_pdftk.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Télécharge le binaire pdftk précompilé
-wget https://github.com/sidorares/pdftk/releases/download/v2.2.1/pdftk-2.2.1-linux-x86_64.tar.gz
+wget https://github.com/pdftk-java/pdftk/releases/download/v4.2.0/pdftk-4.2.0-linux-amd64.tar.gz
 
 # Extrait le contenu de l'archive
-tar -xzf pdftk-2.2.1-linux-x86_64.tar.gz
+tar -xzf pdftk-4.2.0-linux-amd64.tar.gz
 
 # Copie le binaire pdftk vers un emplacement accessible
-cp pdftk-2.2.1-linux-x86_64/bin/pdftk ./bin/
+cp pdftk-4.2.0-linux-amd64/bin/pdftk ./bin/
 
 # Nettoyage des fichiers temporaires
-rm -rf pdftk-2.2.1-linux-x86_64 pdftk-2.2.1-linux-x86_64.tar.gz
+rm -rf pdftk-4.2.0-linux-amd64 pdftk-4.2.0-linux-amd64.tar.gz
 
 echo "pdftk a été installé avec succès."
