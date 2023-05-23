d56c6ea4135c5772f579fee845bcc348b0c74b84
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-05-23 12:29:26 +0200

    package

--- script.js
+++ script.js
@@ -1,54 +1,10 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-    const formData = new FormData(form);
-    fetch('/upload', {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-});
-let inputexcelfile = document.getElementById("excelfile");
-let exceltable = document.getElementById("exceltable");
-inputexcelfile.addEventListener('change', (event) => {
+const path = require('path');
+const fs = require('fs-extra');
 
-    const file = event.target.files[0];
-    console.log(file)
-    readXlsxFile(file).then((rows) => {
-        exceltable.innerHTML = "";
-        rows.forEach((row) => {
-            let tr = document.createElement('tr');
-            row.forEach((cell) => {
+const sourceDir = path.join(__dirname,'netlify' ,'functions', 'pdf-gen', 'bin');
+console.log(process.env['LAMBDA_TASK_ROOT'])
+const destinationDir = path.join(process.env.LAMBDA_TASK_ROOT, 'src', 'bin');
 
-                const td = document.createElement('td');
-                if (Object.prototype.toString.call(cell) === '[object Date]') {
-                    const date = cell;
-                    const offset = date.getTimezoneOffset();
-                    offset === -60 ? date.setHours(date.getHours() + 11) : date.setHours(date.getHours() + 10);
-                    const options = {
-                        timeZone: "Europe/Paris",
-                        hour12: false,
-                        hour: "2-digit",
-                        minute: "2-digit"
-                    };
-                    td.textContent = date.toLocaleTimeString("fr-FR", options);
-                }else{
-                    const regex = /CAP AMPERE-MUT-(.{7})/;
-                    const result = regex.exec(cell);
+fs.copySync(sourceDir, destinationDir);
 
-                    if (cell.includes(" - externe")) {
-                        cell = cell.replace(" - externe", "");
-                    }
-                    result ? td.textContent = result[1] : td.textContent = cell
-                }
-                tr.appendChild(td);
-            })
-            exceltable.appendChild(tr);
-        })
-    })
-})
+console.log('Binaries moved successfully!');
